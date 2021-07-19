eae8f059b842c35ffc4a374abe5faaa5de565593
@nder1996 <72629491+nder1996@users.noreply.github.com>
2021-07-19 13:10:22 -0500

    V1.0

---
+++ 1.js
@@ -0,0 +1,26 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+
+
+var firebaseConfig = {
+    apiKey: "AIzaSyBU0Tbmu5B_sO2k-N2CX6gD16F8KFFPt7g",
+    authDomain: "bd-seminario.firebaseapp.com",
+    databaseURL: "https://bd-seminario-default-rtdb.firebaseio.com",
+    projectId: "bd-seminario",
+    storageBucket: "bd-seminario.appspot.com",
+    messagingSenderId: "437801277158",
+    appId: "1:437801277158:web:5e5ef674dc0bdc54338d27",
+    measurementId: "G-N1D2TJ3FBS"
+};
+
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+var starCountRef = firebase.database().ref('posts/' + postId + '/starCount');
+starCountRef.on('value', (snapshot) => {
+  const data = snapshot.val();
+  updateStarCount(postElement, data);
+});
