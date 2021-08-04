807090df71c5bcbb54edb6d37fbfada7203f2662
@nder1996 <72629491+nder1996@users.noreply.github.com>
2021-08-03 22:42:52 -0500

    CRUD_COMPLETO

--- 1.js
+++ 1.js
@@ -67,12 +67,12 @@ function Agregar_Estudiante() {
 
         firebase.database().ref('Formulario_Registro/' + Username).set({
 
-            Apellido: Apellido,
-            Contraseña: Contraseña_1,
-            Email: Email,
-            Grado: Grado,
-            Username: Username,
-            Nombre: Nombre,
+            Apellido: Eliminar_Inncesario(Apellido),
+            Contraseña: Eliminar_Inncesario(Contraseña_1),
+            Email: Eliminar_Inncesario(Email),
+            Grado: Eliminar_Inncesario(Grado),
+            Username: Eliminar_Inncesario(Username),
+            Nombre: Eliminar_Inncesario(Nombre),
 
         });
 
@@ -97,11 +97,140 @@ function Agregar_Estudiante() {
 
 }
 
-function Eliminar_Estudiante(Username){
+function Eliminar_Estudiante(Username) {
 
-        firebase.database().ref('Formulario_Registro/' + Username).remove();
-        window.location.reload();
+    console.log("USUARIO A ELIMINAR : ",Username)
+
+    firebase.database().ref('Formulario_Registro/' + Username).remove();
+    window.location.reload();
+
 }
+
+function Editar_Estudiante(Username) {
+
+   
+
+
+    //const Formulario = firebase.database().ref();
+
+
+    firebase.database().ref().child("Formulario_Registro/" + Username).on('value', snap => {
+
+        let User = snap.val();
+
+        $("#Nombre_Edit").val(User.Nombre);
+        $("#Apellido_Edit").val(User.Apellido);
+        $("#Grado_Edit").val(User.Grado);
+        $("#Email_Edit").val(User.Email);
+        $("#Username_Edit").val(User.Username);
+        $("#Contraseña_Edit_1").val(User.Contraseña);
+        $("#Contraseña_Edit_2").val(User.Contraseña);
+        
+        });
+
+        /*if(User.Username!=null && User.Username!=undefined){
+
+             console.log("ESTE ES USERNAME : ", User.Username );
+        }
+
+        
+        
+        /*$("#Apellido_Edit").val(User.Apellido).val();
+        $("#Grado_Edit").val(User.Grado).val();
+        $("#Email_Edit").val(User.Email).val();
+        $("#Username_Edit").val(User.Username).val();
+        $("#Contraseña_Edit").val(User.Contraseña).val();*/
+
+  
+
+
+    /*firebase.database().ref('Formulario_Registro/'+Username).once('value').then(function(snapshot){
+
+      /*  document.getElementById("Nombre_Edit").value = snapshot.val().Nombre;
+        document.getElementById("Apellido_Edit").value = snapshot.val().Apellido;
+        document.getElementById("Grado_Edit").value = snapshot.val().Grado;
+        document.getElementById("Email_Edit").value = snapshot.val().Email;*/
+        //document.getElementById("Username_Edit").value =  snapshot.val().Username;
+        /*document.getElementById("Contraseña_Edit_1").value = snapshot.val().Contraseña;
+        document.getElementById("Contraseña_Edit_2").value = snapshot.val().Contraseña;*//*
+    });*/
+
+
+
+}
+
+function Actualizar_Datos(Key_Estudiante) {
+
+
+
+
+    Nombre = $("#Nombre_Edit").val();
+    Apellido = $("#Apellido_Edit").val();
+    Grado = $("#Grado_Edit").val();
+    Email = $("#Email_Edit").val();
+    Username_1 = $("#Username_Edit").val();
+    Contraseña_1 = $("#Contraseña_Edit_1").val();
+    Contraseña_2 = $("#Contraseña_Edit_2").val();
+
+    Validar_Nombre = /^[a-zA-Z]+[a-zA-Z]+$/;
+    Validar_Apellido = /^[a-zA-ZÀ-ÿ\s]{3,15}$/;
+    Validar_Email = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
+    Validar_Password = /^[A-Za-z0-9]{8,30}$/;
+    Validar_Username = /^[a-z0-9_\\_\ü]+$/;
+
+
+
+    if (Validar_Nombre.test(Nombre) == true && Validar_Apellido.test(Apellido) == true && Validar_Email.test(Email) == true && Validar_Password.test(Contraseña_1) == true && Validar_Password.test(Contraseña_2) == true && Validar_Username.test(Username_1) == true) {
+
+           
+
+        firebase.database().ref('Formulario_Registro/' + Username_1).update({
+            Apellido: Apellido,
+            Contraseña: Contraseña_1,
+            Email: Email,
+            Grado: Grado,
+            Username: Username_1,
+            Nombre: Nombre,
+        });
+
+
+        window.location.reload();
+
+            
+            //Eliminar_Estudiante(Key_Estudiante);
+
+       // Eliminar_Estudiante(Key_Estudiante);
+        
+
+
+
+        Swal.fire({
+            title: 'Los Datos Han Sido Editados',
+            showClass: {
+                popup: 'animate__animated animate__fadeInDown'
+            },
+            hideClass: {
+                popup: 'animate__animated animate__fadeOutUp'
+            }
+        })
+
+
+
+    } else {
+
+        console.log("Nombre : ",Nombre) ; console.log("Apellido : ",Apellido) ; console.log("Grado : ",Grado) ; console.log("Email : ",Email) ;console.log("Username : ",Username_1) ;
+
+        console.log("Contraseña_1 : ",Contraseña_1) ;console.log("Contraseña_2 : ",Contraseña_2) ;
+
+
+        Swal.fire({
+            icon: 'error',
+            title: 'Ingresa Datos Correctos!',
+        })
+    }
+
+}
+
 
 
 
@@ -130,7 +259,7 @@ function Leer_Datos() {
                                             <div class="modal-dialog">
                                                 <div class="modal-content">
                                                     <div class="modal-header">
-                                                        <h5 class="modal-title m-auto" id="exampleModalLabel">Ingresar Nuevo Estudiante</h5>
+                                                        <h5 class="modal-title m-auto" id="exampleModalLabel">Modifica Los Datos Del Estudiante</h5>
                                                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                                     </div>
                                                     <div class="modal-body">
@@ -145,20 +274,21 @@ function Leer_Datos() {
                                                                     <option value="Septimo de bachillerato">Septimo de bachillerato</option>
                                                                     <option value="Octavo de bachillerato">Octavo de bachillerato</option>
                                                                 </select>
-                                                                <input id="Email_Edit" type="text" class="form-control" id="recipient-name" placeholder="Email" required>
-                                                                <input id="Username_Edit" type="text" class="form-control" id="recipient-name" placeholder="Username" required>
-                                                                <input id="Contraseña_Edit" type="text" class="form-control" id="recipient-name" placeholder="Contraseña" required>
+                                                                <input id="Email_Edit" type="text" class="form-control"  placeholder="Email" required>
+                                                                <input id="Username_Edit" type="text" class="form-control"  placeholder="Username" required>
+                                                                <input id="Contraseña_Edit_1" type="text" class="form-control" placeholder="Password" required>
+                                                                <input id="Contraseña_Edit_2" type="text" class="form-control" placeholder="Confirm Password" required>
                                                             </div>
                                                         </form>
                                                     </div>
                                                     <div class="modal-footer">
                                                         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
-                                                        <button type="button" class="btn btn-primary" >Actualizar Datos</button>
+                                                        <button type="button" class="btn btn-primary" onclick=Actualizar_Datos('${Eliminar_Inncesario(User.Username)}')>Actualizar Datos</button>
                                                     </div>
                                                 </div>
                                             </div>
                                         </div>
-                       <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#Edit_Estudiante" data-bs-whatever="@mdo" id="Edit_${Contador}" onclick=Editar_Estudiante(Eliminar_Inncesario('${Eliminar_Inncesario(User.Username)}'))>Editar</button>
+                       <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#Edit_Estudiante" data-bs-whatever="@mdo" id="Edit_${Contador}" onclick=Editar_Estudiante('${User.Username}')>Editar</button>
                     <button type="button" class="btn btn-danger" id="Elim_${Contador}" onclick=Eliminar_Estudiante('${Eliminar_Inncesario(User.Username)}')>Eliminar</button>
                 </th>
             </tr>
